c19b00a8c79e9ed777f58b878090e240393d779f
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-03-27 16:02:49 +0900

    ⛓ Connect control views

--- ch05-programmingView/ch05-1871408-programmingView/ConversionViewController.swift
+++ ch05-programmingView/ch05-1871408-programmingView/ConversionViewController.swift
@@ -21,6 +21,14 @@ extension ConversionViewController {
         isLabel = createLabel("is", fontSize: 36)
         isLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         isLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        
+        fahrenheitTextField = createTextField(placeHolder: "VALUE")
+        celsiusLabel = createLabel("???", fontSize: 70)
+        fdegreeLabel = createLabel("degrees Fahrenheit", fontSize: 36)
+        cdegreeLabel = createLabel("degrees Celsius", fontSize: 36)
+        
+        connectVertically(views: fahrenheitTextField, fdegreeLabel, isLabel, celsiusLabel, cdegreeLabel, spacing: 8)
+        connectHorizontally(views: [fahrenheitTextField, fdegreeLabel, isLabel, celsiusLabel, cdegreeLabel])
     }
 }
 
@@ -44,9 +52,26 @@ extension ConversionViewController {
         label.text = text
         label.textColor = UIColor(red: CGFloat(0xe1)/CGFloat(256), green: CGFloat(0x58)/CGFloat(256), blue: CGFloat(0x29)/CGFloat(256), alpha: CGFloat(1))
         label.textAlignment = .center
+        label.font = UIFont(name: label.font.fontName, size: fontSize)
         label.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(label)
         return label
+    }
+}
+
+extension ConversionViewController {
+    func connectVertically(views: UIView..., spacing: CGFloat) {
+        for i in 0..<views.count - 1 {
+            views[i].bottomAnchor.constraint(equalTo: views[i + 1].topAnchor, constant: spacing).isActive = true
+        }
+    }
+}
+
+extension ConversionViewController {
+    func connectHorizontally(views: [UIView]) {
+        for view in views {
+            view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        }
     }
 }
 
