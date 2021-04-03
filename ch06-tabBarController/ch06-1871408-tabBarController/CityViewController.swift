d38869009a128dd8144d1b1138fd73b261eb6eb4
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-04-03 15:02:41 +0900

    📱 Setup view controller

---
+++ ch06-tabBarController/ch06-1871408-tabBarController/CityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ch06-1871408-tabBarController
+//
+//  Created by JeongHyeon Lee on 2021/04/03.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("CityViewController.viewDidLoad")
+    }
+
+
+}
+
---
+++ ch06-tabBarController/ch06-1871408-tabBarController/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  ch06-1871408-tabBarController
+//
+//  Created by JeongHyeon Lee on 2021/04/03.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
