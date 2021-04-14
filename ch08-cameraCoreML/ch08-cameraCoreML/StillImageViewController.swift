7450ed04a44b5aea73151706343871adea7bd2f4
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-04-14 17:46:31 +0900

    🎞 Creating image classifier

--- ch08-cameraCoreML/ch08-cameraCoreML/StillImageViewController.swift
+++ ch08-cameraCoreML/ch08-cameraCoreML/StillImageViewController.swift
@@ -6,21 +6,27 @@
 //
 
 import UIKit
+import Vision
+import CoreML
 
-class StillImageViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class StillImageViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    
+    var request: VNCoreMLRequest!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(takePicture))
         imageView.addGestureRecognizer(tapGesture)
+        imageView.isUserInteractionEnabled = true
         
         messageLabel.layer.borderWidth = 2
         messageLabel.layer.borderColor = UIColor.red.cgColor
-        // 반드시 설정하여야 한다
-        imageView.isUserInteractionEnabled = true
+        
+        request = createCoreMLRequest(modelName: "SqueezeNet", modelExt: "mlmodelc", completionHandler: handleImageClassifier)
     }
 }
 
@@ -44,15 +50,40 @@ extension StillImageViewController {
     // 사진을 캡쳐하는 경우 호출하는 함수
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         // 사진을 가져온다
-        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
-        // 가져온 사진을 UIImageView에 나타나도록 한다
-        imageView.image = image
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            
+            let handler = VNImageRequestHandler(ciImage: CIImage(image: image)!)
+            try! handler.perform([request])
+        }
         picker.dismiss(animated: true, completion: nil)
     }
     
     // 사진 캡쳐를 취소하는 경우 호출하는 함수
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension StillImageViewController {
+    func createCoreMLRequest(modelName: String, modelExt: String, completionHandler: @escaping (VNRequest, Error?) -> Void) -> VNCoreMLRequest? {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: modelExt) else {
+            return nil
+        }
+        guard let vnCoreMLModel = try? VNCoreMLModel(for: MLModel(contentsOf: modelURL)) else {
+            return nil
+        }
+        return VNCoreMLRequest(model: vnCoreMLModel, completionHandler: completionHandler)
+    }
+    
+    func handleImageClassifier(request: VNRequest, error: Error?) {
+        guard let results = request.results as? [VNClassificationObservation] else {
+            return
+        }
+        if let topResult = results.first {
+            DispatchQueue.main.async {
+                self.messageLabel.text = "\(topResult.identifier)입니다. 아무 곳이나 클릭하세요"
+            }
+        }
     }
 }
