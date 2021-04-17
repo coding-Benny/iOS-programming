01ef4f4611f871b380a1e15710224d6d5c713240
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-04-17 17:12:56 +0900

    📋 Create table view list

---
+++ ch09-tableView/ch09-1871408-tableView/UserGroupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ch09-1871408-tableView
+//
+//  Created by JeongHyeon Lee on 2021/04/18.
+//
+
+import UIKit
+
+class UserGroupViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
