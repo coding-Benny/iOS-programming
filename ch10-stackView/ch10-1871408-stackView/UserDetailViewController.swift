ae0ee54f73fcd875793134f50ef692a58073e94c
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2021-05-02 14:55:19 +0900

    👤 Create user interface

--- ch10-stackView/ch10-1871408-stackView/UserDetailViewController.swift
+++ ch10-stackView/ch10-1871408-stackView/UserDetailViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class UserDetailViewController: UIViewController {
+    
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwdTextField: UITextField!
+    @IBOutlet weak var facilityTableView: UITableView!
+    
+    var user: User?
+}
+
+extension UserDetailViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        user = user ?? User(random: true)
+        idTextField.text = user!.id
+        nameTextField.text = user!.name
+        passwdTextField.text = user!.passwd
+    }
+}
+
+extension UserDetailViewController {
+    @IBAction func dismissUserViewController(_ sender: UIButton) {
     }
 }
